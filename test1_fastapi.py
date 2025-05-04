391d57ca098c901eade8ec7b762837cb39a1ea52
Wadangzz <151944861+Wadangzz@users.noreply.github.com>
2025-05-04 20:23:25 +0900

    Update test1_fastapi.py
    DB Server에 과부하가 안 생기도록 Query execute를 한번만 하는 방향으로 수정
    잘 작동하는지는 Test 해봐야 하고 메서드, 클래스 리팩토링은 추후에 다시

--- test1_fastapi.py
+++ test1_fastapi.py
@@ -32,6 +32,7 @@ def video_feed():
 def start_fastapi(app):
     uvicorn.run(app, host="0.0.0.0", port=9000)
 
+
 if __name__ == "__main__":
     
     inspection_thread = threading.Thread(target=start_fastapi,args = (app,),daemon=True)
@@ -50,16 +51,18 @@ if __name__ == "__main__":
         database = 'product_db') as conn:
 
         ng = {
-            0: {"id": 'Dust'},
-            1: {"id": 'Scratch'}
+            0: {"id": 'Dust',
+                "detected" : False},
+            1: {"id": 'Scratch',
+                "detected" : False}
         }
 
-        inspect_count = 0
+        # inspect_count = 0
         inspected = False
 
         INSPECTION_MODE = 36
         INSPECTION_COMPLETE = 37
-        MAX_INSPECT_COUNT = 10
+        # MAX_INSPECT_COUNT = 10
 
         model = YOLO('./runs/detect/project2_1/weights/best.pt')
         cap = cv2.VideoCapture(0)
@@ -75,13 +78,12 @@ if __name__ == "__main__":
             sharpening = cv2.addWeighted(frame, 1.5, img, -0.5, 0)
             results = model.predict(frame, conf= 0.65, iou=0.5)
             boxes = results[0].boxes
-            print(boxes.cls.cpu().numpy())
             
             cv2.rectangle(frame, (roi[0], roi[1]), (roi[2], roi[3]), (0, 255, 255), 2)
             # D16이 36일 때 검사 진행 중
-            status = pymc3e.batchread_wordunits(headdevice="D16",readsize=1) 
+            status = pymc3e.batchread_wordunits(headdevice="D16",readsize=1)[0] 
 
-            if status[0] == INSPECTION_MODE and inspect_count < MAX_INSPECT_COUNT:
+            if status == INSPECTION_MODE:# and inspect_count < MAX_INSPECT_COUNT:
                 with conn.cursor() as cursor:
                     col_name = None
                     if boxes.cls.numel() > 0: # 객체가 1개라도 감지되면
@@ -89,14 +91,17 @@ if __name__ == "__main__":
                             if is_center_in_roi(box,roi): # 객체의 boungind box가 roi 안에 있다면
                                 pymc3e.batchwrite_wordunits(headdevice="D2001", values=[1]) # PLC D2001에 1을 쓴다
                         for cls in boxes.cls.cpu().numpy(): # cls ID를 numpy 배열로 변환
-                            if cls == 0:
+                            if cls == 0 and ng[0]["detected"] == False:
                                 col_name = ng[0]["id"] # Dust
-                            elif cls == 1:
+                                ng[0]["detected"] = True
+                            elif cls == 1 and ng[1]["detected"] == False:
                                 col_name = ng[1]["id"] # Scratch
-                            cursor.execute(
-                                f"""
-                                UPDATE productnum SET {col_name} = %s
-                                WHERE inspection = 'Not Yet' AND {col_name} = 0 ORDER BY id ASC LIMIT 1""", (1,))
+                                ng[1]["detected"] = True
+                            if not col_name == None:
+                                cursor.execute(
+                                    f"""
+                                    UPDATE productnum SET {col_name} = %s
+                                    WHERE inspection = 'Not Yet' AND {col_name} = 0 ORDER BY id ASC LIMIT 1""", (1,))
                         cursor.execute(
                             """
                             UPDATE productnum SET inspection = %s 
@@ -110,28 +115,32 @@ if __name__ == "__main__":
                             WHERE inspection = 'Not Yet' ORDER BY id ASC LIMIT 1""", ('OK',))
                         conn.commit()
 
-                inspect_count += 1
+                # inspect_count += 1
 
-            if status[0] == INSPECTION_COMPLETE and inspected == False:
-                inspect_count = 0
+            if status == INSPECTION_COMPLETE and inspected == False:
+                # inspect_count = 0
+                ins_result = None
+                for i in range(2):
+                    ng[i]["detected"] = False # 객체 감지 결과를 리셋
                 inspected = True
-                inspection = pymc3e.batchread_wordunits(headdevice="D2001",readsize=1)
-                if inspection[0] == 0:
+                inspection_result = pymc3e.batchread_wordunits(headdevice="D2001",readsize=1)[0]
+                if inspection_result == 0: # 양품
                     ins_result = 'OK'
-                elif inspection[0] == 1:
+                elif inspection_result == 1: # 불량
                     ins_result = 'NG'
-                with conn.cursor() as cursor:
-                    cursor.execute(
-                        f"""
-                        INSERT INTO {ins_result} 
-                        (ProductCode, Model, Dust, Scratch, inspection)
-                        SELECT ProductCode, Model, Dust, Scratch, inspection
-                        FROM productnum WHERE inspection = %s LIMIT 1""", (ins_result,))
-                    cursor.execute(    
-                        "DELETE FROM productnum WHERE inspection = %s LIMIT 1", (ins_result,))
-                    conn.commit()
+                if not ins_result == None:
+                    with conn.cursor() as cursor: # 검사 결과에 따라 해당 제품코드의 정보를 OK, NG 테이블로 이동
+                        cursor.execute(
+                            f"""
+                            INSERT INTO {ins_result} 
+                            (ProductCode, Model, Dust, Scratch, inspection)
+                            SELECT ProductCode, Model, Dust, Scratch, inspection
+                            FROM productnum WHERE inspection = %s LIMIT 1""", (ins_result,))
+                        cursor.execute(    
+                            "DELETE FROM productnum WHERE inspection = %s LIMIT 1", (ins_result,))
+                        conn.commit()
 
-            if status[0] != INSPECTION_COMPLETE and inspected:
+            if status != INSPECTION_COMPLETE and inspected: # 검사 완료되어 후공정 이동
                 inspected = False
 
             annotated_image = results[0].plot()
